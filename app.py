4915bde2e9f5f0fc2bccd212822e7c61e7cb8051
Bodor Máté <bodor.mate@kszk.bme.hu>
2019-04-14 18:22:38 +0200

    workshop

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from flask import render_template
+from flask import redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    done = db.Column(db.Boolean, default=False)
+
+    def __init__(self, content):
+        self.content = content
+        self.done = False
+
+    def __repr__(self):
+        return "<Content %s>" % self.content
+
+
+db.create_all()
+
+
+@app.route("/")
+def tasks_list():
+    return render_template("list.html", name="World")
+
+
+if __name__ == "__main__":
+    app.run()
