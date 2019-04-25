e7a6925c958b02cd298d337a6aeb85bc1815175e
tutkovics <tutkovics@sch.bme.hu>
2019-04-25 17:48:04 +0200

    Can delete task

--- app.py
+++ app.py
@@ -24,10 +24,47 @@ class Task(db.Model):
 
 db.create_all()
 
-
+# landing page
 @app.route("/")
 def tasks_list():
-    return render_template("list.html", name="World")
+    tasks = Task.query.all()
+    return render_template("list.html", tasks=tasks)
+
+
+# create new task
+@app.route("/task", methods=["POST"])
+def add_task():
+    content = request.form["content"]
+    if not content:
+        return "Hiba"
+    task = Task(content)
+    db.session.add(task)
+    db.session.commit()
+    return redirect("/")
+
+
+# done a task
+@app.route("/done/<int:task_id>")
+def resolve_task(task_id):
+    task = Task.query.get(task_id)
+    if not task:
+        return redirect("/")
+    if task.done:
+        task.done = False
+    else:
+        task.done = True
+    db.session.commit()
+    return redirect("/")
+
+# delete a task
+@app.route("/delete/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    if not task:
+        return redirect("/")
+    db.session.delete(task)
+    db.session.commit()
+    return redirect("/")
 
 
 if __name__ == "__main__":
